7203c343446cf5e1f50e50e6d0160a0e7539be80
cayceevans213 <cevangelista2021@student.nbscollege.edu.ph>
2023-10-19 21:52:45 +0800

    Addded homepage to library system

---
+++ script.js
@@ -0,0 +1,24 @@
+const sliderImages = ['images/image1.jpg', 'images/image2.jpg', 'images/image3.jpg'];
+const sliderImage = document.getElementById('sliderImage');
+let currentImageIndex = 0;
+
+function updateSliderImage() {
+    sliderImage.style.backgroundImage = `url(${sliderImages[currentImageIndex]}`;
+}
+
+function nextImage() {
+    currentImageIndex = (currentImageIndex + 1) % sliderImages.length;
+    updateSliderImage();
+}
+
+function prevImage() {
+    currentImageIndex = (currentImageIndex - 1 + sliderImages.length) % sliderImages.length;
+    updateSliderImage();
+}
+
+const nextButton = document.getElementById('nextButton');
+const prevButton = document.getElementById('prevButton');
+
+
+
+updateSliderImage();
